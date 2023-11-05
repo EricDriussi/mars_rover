6b2d77ecf0fb8ba821b95dd2dc1c4c31a26ecb03
Eric Driussi <ericdriussi@gmail.com>
2023-11-05 11:28:49 +0000

    location tests

--- internal/domain/location/test/location_test.go
+++ internal/domain/location/test/location_test.go
@@ -7,7 +7,56 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 )
+
+func TestGetsPosition(t *testing.T) {
+	coord := *coordinate.NewAbsolute(1, 1)
+	loc, _ := location.From(coord, &direction.North{})
+	assert.Equal(t, coord, loc.Position())
+}
+
+func TestUpdatesPosition(t *testing.T) {
+	loc, _ := location.From(*coordinate.NewAbsolute(1, 1), &direction.North{})
+	loc.CalculatePositionAhead()
+	loc.UpdatePosition()
+	assert.Equal(t, *coordinate.NewAbsolute(1, 2), loc.Position())
+	loc.CalculatePositionBehind()
+	loc.UpdatePosition()
+	assert.Equal(t, *coordinate.NewAbsolute(1, 1), loc.Position())
+}
+
+func TestResetsPosition(t *testing.T) {
+	coord := *coordinate.NewAbsolute(1, 1)
+	loc, _ := location.From(coord, &direction.North{})
+	loc.CalculatePositionAhead()
+	loc.Reset()
+	assert.Equal(t, coord, loc.Position())
+}
+
+func TestFacesLeft(t *testing.T) {
+	expectedDirection := &direction.West{}
+	mockDirection := new(mockDirection)
+	mockDirection.On("DirectionOnTheLeft").Return(expectedDirection)
+	startingLocation, _ := location.From(*coordinate.NewAbsolute(1, 1), mockDirection)
+
+	startingLocation.FaceLeft()
+
+	expectedLocation, _ := location.From(*coordinate.NewAbsolute(1, 1), expectedDirection)
+	assert.Equal(t, expectedLocation, startingLocation)
+}
+
+func TestFacesRight(t *testing.T) {
+	expectedDirection := &direction.East{}
+	mockDirection := new(mockDirection)
+	mockDirection.On("DirectionOnTheRight").Return(expectedDirection)
+	startingLocation, _ := location.From(*coordinate.NewAbsolute(1, 1), mockDirection)
+
+	startingLocation.FaceRight()
+
+	expectedLocation, _ := location.From(*coordinate.NewAbsolute(1, 1), expectedDirection)
+	assert.Equal(t, expectedLocation, startingLocation)
+}
 
 func TestDoesNotAllowNegativeValues(t *testing.T) {
 	testCases := []struct {
@@ -29,30 +78,37 @@ func TestDoesNotAllowNegativeValues(t *testing.T) {
 	}
 	for _, testCase := range testCases {
 		t.Run(testCase.name, func(t *testing.T) {
-			_, err := location.From(*testCase.coordinate, mockDirection{})
+			_, err := location.From(*testCase.coordinate, &mockDirection{})
 			assert.Error(t, err)
 		})
 	}
 }
 
-type mockDirection struct{}
-
-func (this mockDirection) Degree() int {
-	return 420
+type mockDirection struct {
+	mock.Mock
 }
 
-func (this mockDirection) DirectionOnTheLeft() direction.Direction {
-	return this
+func (this *mockDirection) Degree() int {
+	args := this.Called()
+	return args.Int(0)
 }
 
-func (this mockDirection) DirectionOnTheRight() direction.Direction {
-	return this
+func (this *mockDirection) DirectionOnTheLeft() direction.Direction {
+	args := this.Called()
+	return args.Get(0).(direction.Direction)
 }
 
-func (this mockDirection) RelativePositionAhead() coordinate.RelativeCoordinate {
-	return *coordinate.RelativeFrom(0, 0)
+func (this *mockDirection) DirectionOnTheRight() direction.Direction {
+	args := this.Called()
+	return args.Get(0).(direction.Direction)
 }
 
-func (this mockDirection) RelativePositionBehind() coordinate.RelativeCoordinate {
-	return *coordinate.RelativeFrom(0, 0)
+func (this *mockDirection) RelativePositionAhead() coordinate.RelativeCoordinate {
+	args := this.Called()
+	return args.Get(0).(coordinate.RelativeCoordinate)
+}
+
+func (this *mockDirection) RelativePositionBehind() coordinate.RelativeCoordinate {
+	args := this.Called()
+	return args.Get(0).(coordinate.RelativeCoordinate)
 }
--- internal/domain/location/test/location_wrap_test.go
+++ internal/domain/location/test/location_wrap_test.go
@@ -10,8 +10,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-// TODO: mocks?
-func TestWrapsAroundIfOutOfBounds(t *testing.T) {
+func TestWrapsAheadIfOutOfBounds(t *testing.T) {
 	maxValue := 2
 	minValue := 0
 	sizeLimit := size.Size{Width: maxValue, Height: maxValue}
@@ -55,4 +54,64 @@ func TestWrapsAroundIfOutOfBounds(t *testing.T) {
 			assert.Equal(t, *testCase.expected, loc.WillBeAt())
 		})
 	}
+}
+
+func TestDoesNotWrapAheadIfWithinBounds(t *testing.T) {
+	loc, _ := location.From(*coordinate.NewAbsolute(1, 1), &direction.North{})
+	loc.CalculatePositionAhead()
+	loc.WrapAround(size.Size{Width: 2, Height: 2})
+	assert.Equal(t, *coordinate.NewAbsolute(1, 2), loc.WillBeAt())
+}
+
+func TestWrapsBehindIfOutOfBounds(t *testing.T) {
+	maxValue := 2
+	minValue := 0
+	sizeLimit := size.Size{Width: maxValue, Height: maxValue}
+	testCases := []struct {
+		name      string
+		starting  *coordinate.AbsoluteCoordinate
+		direction direction.Direction
+		expected  *coordinate.AbsoluteCoordinate
+	}{
+		{
+			name:      "North",
+			starting:  coordinate.NewAbsolute(1, minValue),
+			direction: &direction.North{},
+			expected:  coordinate.NewAbsolute(1, maxValue),
+		},
+		{
+			name:      "South",
+			starting:  coordinate.NewAbsolute(1, maxValue),
+			direction: &direction.South{},
+			expected:  coordinate.NewAbsolute(1, minValue),
+		},
+		{
+			name:      "East",
+			starting:  coordinate.NewAbsolute(minValue, 1),
+			direction: &direction.East{},
+			expected:  coordinate.NewAbsolute(maxValue, 1),
+		},
+		{
+			name:      "West",
+			starting:  coordinate.NewAbsolute(maxValue, 1),
+			direction: &direction.West{},
+			expected:  coordinate.NewAbsolute(minValue, 1),
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			loc, _ := location.From(*testCase.starting, testCase.direction)
+			loc.CalculatePositionBehind()
+			loc.WrapAround(sizeLimit)
+			assert.Equal(t, *testCase.expected, loc.WillBeAt())
+		})
+	}
+}
+
+func TestDoesNotWrapBehindIfWithinBounds(t *testing.T) {
+	loc, _ := location.From(*coordinate.NewAbsolute(1, 1), &direction.North{})
+	loc.CalculatePositionBehind()
+	loc.WrapAround(size.Size{Width: 2, Height: 2})
+	assert.Equal(t, *coordinate.NewAbsolute(1, 0), loc.WillBeAt())
 }
