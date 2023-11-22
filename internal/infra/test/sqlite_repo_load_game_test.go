ba554c2f1f425342ba6031fe5e830b4ffdf492bf
Eric <eric@leanmind.es>
2023-11-22 17:18:12 +0000

    table testing

--- internal/infra/test/sqlite_repo_load_game_test.go
+++ internal/infra/test/sqlite_repo_load_game_test.go
@@ -3,82 +3,81 @@ package infra_test
 import (
 	"database/sql"
 	"github.com/google/uuid"
+	. "mars_rover/internal/domain/planet"
+	. "mars_rover/internal/domain/rover"
 	. "mars_rover/internal/infra"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestLoadGodModRoverGame(t *testing.T) {
-	db := InitMem()
-	defer func(db *sql.DB) {
-		err := db.Close()
-		if err != nil {
-			panic("err closing db connection")
-		}
-	}(db)
-	testRover, testPlanet := setupGodModRoverOnRockyPlanet()
-	repo := NewSQLite(db)
-	err := repo.SaveGame(testRover, testPlanet)
-	assert.Nil(t, err)
+func TestLoadsRover(t *testing.T) {
+	testCases := []struct {
+		name      string
+		setupFunc func() (Rover, Planet)
+	}{
+		{
+			name:      "wrapping rover on rocky planet",
+			setupFunc: setupWrappingRoverOnRockyPlanet,
+		},
+		{
+			name:      "god mod rover on rocky planet",
+			setupFunc: setupGodModRoverOnRockyPlanet,
+		},
+	}
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			db := InitMem()
+			defer func(db *sql.DB) {
+				err := db.Close()
+				if err != nil {
+					panic("err closing db connection")
+				}
+			}(db)
+			testRover, testPlanet := testCase.setupFunc()
+			repo := NewSQLite(db)
+			err := repo.SaveGame(testRover, testPlanet)
+			assert.Nil(t, err)
 
-	gameDTO, err := repo.LoadGame(testRover.Id())
-	assert.Nil(t, err)
+			gameDTO, err := repo.LoadGame(testRover.Id())
+			assert.Nil(t, err)
 
-	assertRoversAreEqual(t, testRover, gameDTO.Rover)
-	assertPlanetsAreEqual(t, testPlanet, gameDTO.Planet)
+			assertRoversAreEqual(t, testRover, gameDTO.Rover)
+			assertPlanetsAreEqual(t, testPlanet, gameDTO.Planet)
+		})
+	}
 }
 
-func TestNotLoadGodModRoverGameWhenIdIsIncorrect(t *testing.T) {
-	db := InitMem()
-	defer func(db *sql.DB) {
-		err := db.Close()
-		if err != nil {
-			panic("err closing db connection")
-		}
-	}(db)
-	testRover, testPlanet := setupGodModRoverOnRockyPlanet()
-	repo := NewSQLite(db)
-	err := repo.SaveGame(testRover, testPlanet)
-	assert.Nil(t, err)
+func TestDoesNotLoadRover(t *testing.T) {
+	testCases := []struct {
+		name      string
+		setupFunc func() (Rover, Planet)
+	}{
+		{
+			name:      "wrapping rover on rocky planet",
+			setupFunc: setupWrappingRoverOnRockyPlanet,
+		},
+		{
+			name:      "god mod rover on rocky planet",
+			setupFunc: setupGodModRoverOnRockyPlanet,
+		},
+	}
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			db := InitMem()
+			defer func(db *sql.DB) {
+				err := db.Close()
+				if err != nil {
+					panic("err closing db connection")
+				}
+			}(db)
+			testRover, testPlanet := testCase.setupFunc()
+			repo := NewSQLite(db)
+			err := repo.SaveGame(testRover, testPlanet)
+			assert.Nil(t, err)
 
-	_, err = repo.LoadGame(uuid.New())
-	assert.NotNil(t, err)
-}
-
-func TestLoadWrappingRoverGame(t *testing.T) {
-	db := InitMem()
-	defer func(db *sql.DB) {
-		err := db.Close()
-		if err != nil {
-			panic("err closing db connection")
-		}
-	}(db)
-	testRover, testPlanet := setupWrappingRoverOnRockyPlanet()
-	repo := NewSQLite(db)
-	err := repo.SaveGame(testRover, testPlanet)
-	assert.Nil(t, err)
-
-	gameDTO, err := repo.LoadGame(testRover.Id())
-	assert.Nil(t, err)
-
-	assertRoversAreEqual(t, testRover, gameDTO.Rover)
-	assertPlanetsAreEqual(t, testPlanet, gameDTO.Planet)
-}
-
-func TestNotLoadWrappingRoverGameWhenIdIsIncorrect(t *testing.T) {
-	db := InitMem()
-	defer func(db *sql.DB) {
-		err := db.Close()
-		if err != nil {
-			panic("err closing db connection")
-		}
-	}(db)
-	testRover, testPlanet := setupWrappingRoverOnRockyPlanet()
-	repo := NewSQLite(db)
-	err := repo.SaveGame(testRover, testPlanet)
-	assert.Nil(t, err)
-
-	_, err = repo.LoadGame(uuid.New())
-	assert.NotNil(t, err)
+			_, err = repo.LoadGame(uuid.New())
+			assert.NotNil(t, err)
+		})
+	}
 }
--- internal/infra/test/sqlite_repo_update_rover_test.go
+++ internal/infra/test/sqlite_repo_update_rover_test.go
@@ -2,50 +2,48 @@ package infra_test
 
 import (
 	"database/sql"
+	. "mars_rover/internal/domain/planet"
+	. "mars_rover/internal/domain/rover"
 	. "mars_rover/internal/infra"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestUpdateWrappingRover(t *testing.T) {
-	db := InitMem()
-	defer func(db *sql.DB) {
-		err := db.Close()
-		if err != nil {
-			panic("err closing db connection")
-		}
-	}(db)
-	testRover, testPlanet := setupWrappingRoverOnRockyPlanet()
-	repo := NewSQLite(db)
+func TestUpdatesRover(t *testing.T) {
+	testCases := []struct {
+		name      string
+		setupFunc func() (Rover, Planet)
+	}{
+		{
+			name:      "wrapping rover on rocky planet",
+			setupFunc: setupWrappingRoverOnRockyPlanet,
+		},
+		{
+			name:      "god mod rover on rocky planet",
+			setupFunc: setupGodModRoverOnRockyPlanet,
+		},
+	}
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			db := InitMem()
+			defer func(db *sql.DB) {
+				err := db.Close()
+				if err != nil {
+					panic("err closing db connection")
+				}
+			}(db)
+			testRover, testPlanet := testCase.setupFunc()
+			repo := NewSQLite(db)
 
-	err := repo.SaveGame(testRover, testPlanet)
-	assert.Nil(t, err)
-	testRover.TurnRight()
-	err = repo.UpdateRover(testRover)
-	assert.Nil(t, err)
+			err := repo.SaveGame(testRover, testPlanet)
+			assert.Nil(t, err)
+			testRover.TurnRight()
+			err = repo.UpdateRover(testRover)
+			assert.Nil(t, err)
 
-	foundRover, err := getLastPersistedRover(db, testPlanet)
-	assertRoversAreEqual(t, foundRover, testRover)
-}
-
-func TestUpdateGodModRover(t *testing.T) {
-	db := InitMem()
-	defer func(db *sql.DB) {
-		err := db.Close()
-		if err != nil {
-			panic("err closing db connection")
-		}
-	}(db)
-	testRover, testPlanet := setupGodModRoverOnRockyPlanet()
-	repo := NewSQLite(db)
-
-	err := repo.SaveGame(testRover, testPlanet)
-	assert.Nil(t, err)
-	testRover.TurnRight()
-	err = repo.UpdateRover(testRover)
-	assert.Nil(t, err)
-
-	foundRover, err := getLastPersistedRover(db, testPlanet)
-	assertRoversAreEqual(t, foundRover, testRover)
+			foundRover, err := getLastPersistedRover(db, testPlanet)
+			assertRoversAreEqual(t, foundRover, testRover)
+		})
+	}
 }
