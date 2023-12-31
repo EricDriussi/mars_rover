a4d5ddb2c36840c6439708c22cbc4df8740e6624
Eric Driussi <ericdriussi@gmail.com>
2023-12-31 13:15:37 +0000

    improve small rock obstacle

--- src/domain/obstacle/smallRock/small_rock_test.go
+++ src/domain/obstacle/smallRock/small_rock_test.go
@@ -4,6 +4,7 @@ import (
 	"mars_rover/src/domain/coordinate/absoluteCoordinate"
 	. "mars_rover/src/domain/coordinate/absoluteCoordinate"
 	"mars_rover/src/domain/obstacle/smallRock"
+	. "mars_rover/src/domain/obstacle/smallRock"
 	"mars_rover/src/domain/size"
 	"math/rand"
 	"testing"
@@ -26,47 +27,46 @@ func TestIsWithinLimit(t *testing.T) {
 func TestIsBeyondLimit(t *testing.T) {
 	sizeLimit, _ := size.Square(3)
 	testCases := []struct {
-		name       string
-		coordinate *AbsoluteCoordinate
+		name string
+		rock SmallRock
 	}{
 		{
-			name:       "both out of bounds",
-			coordinate: absoluteCoordinate.Build(4, 4),
+			name: "both out of bounds",
+			rock: smallRock.In(*absoluteCoordinate.Build(4, 4)),
 		},
 		{
-			name:       "X out of bounds",
-			coordinate: absoluteCoordinate.Build(4, 3),
+			name: "X out of bounds",
+			rock: smallRock.In(*absoluteCoordinate.Build(4, 3)),
 		},
 		{
-			name:       "Y out of bounds",
-			coordinate: absoluteCoordinate.Build(3, 4),
+			name: "Y out of bounds",
+			rock: smallRock.In(*absoluteCoordinate.Build(3, 4)),
 		},
 	}
 	for _, testCase := range testCases {
 		t.Run(testCase.name, func(t *testing.T) {
-			rock := smallRock.In(*testCase.coordinate)
-			assert.True(t, rock.IsBeyond(*sizeLimit))
+			assert.True(t, testCase.rock.IsBeyond(*sizeLimit))
 		})
 	}
 }
 
-func TestOccupiesAGivenRandomCoordinate(t *testing.T) {
-	testCoordinate := absoluteCoordinate.Build(rand.Int(), rand.Int())
-	rock := smallRock.In(*testCoordinate)
+func TestOccupiesItsCoordinate(t *testing.T) {
+	testCoordinate := *absoluteCoordinate.Build(rand.Int(), rand.Int())
+	rock := smallRock.In(testCoordinate)
 
-	assert.True(t, rock.Occupies(*testCoordinate))
+	assert.True(t, rock.Occupies(testCoordinate))
 }
 
-func TestDoesNotOccupyADifferentCoordinate(t *testing.T) {
-	testCoordinate := absoluteCoordinate.Build(1, 1)
-	rock := smallRock.In(*testCoordinate)
+func TestDoesNotOccupyAnExternalCoordinate(t *testing.T) {
+	testCoordinate := *absoluteCoordinate.Build(1, 1)
+	rock := smallRock.In(testCoordinate)
 
 	assert.False(t, rock.Occupies(*absoluteCoordinate.Build(1, 2)))
 }
 
-func TestGetCoordinates(t *testing.T) {
-	testCoordinate := absoluteCoordinate.Build(2, 1)
-	rock := smallRock.In(*testCoordinate)
+func TestListsOccupiedCoordinates(t *testing.T) {
+	testCoordinate := *absoluteCoordinate.Build(2, 1)
+	rock := smallRock.In(testCoordinate)
 
-	assert.Equal(t, rock.Coordinates(), []AbsoluteCoordinate{*testCoordinate})
+	assert.Equal(t, rock.Coordinates(), []AbsoluteCoordinate{testCoordinate})
 }
