8bfa067c94b531089e6b215090c9951eebd67ec2
Eric <eric@leanmind.es>
2024-01-11 17:39:42 +0000

    wire up api handler

--- src/infra/apiServer/server.go
+++ src/infra/apiServer/server.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	. "mars_rover/src/action"
 	"mars_rover/src/action/createRandom/bounded"
+	"mars_rover/src/action/load"
 	"mars_rover/src/action/move/resilient"
 	. "mars_rover/src/infra/apiServer/controllers"
 	. "mars_rover/src/infra/apiServer/responses"
@@ -14,18 +15,20 @@ import (
 	"sync"
 )
 
-var movementAction MoveAction
 var creationAction CreateRandomAction
+var movementAction MoveAction
+var loadAction LoadAction
 
 func RunOn(port string, wg *sync.WaitGroup) {
 	defer wg.Done()
-	movementAction = resilient_mover.With(InitFS())
 	creationAction = boundedRandomCreator.With(InitFS())
+	movementAction = resilient_mover.With(InitFS())
+	loadAction = load.With(InitFS())
 
 	apiServer := http.NewServeMux()
-	// TODO: add load game endpoint
 	apiServer.HandleFunc("/api/randomGame", randomGameHandler)
 	apiServer.HandleFunc("/api/moveSequence", moveSequenceHandler)
+	apiServer.HandleFunc("/api/loadGame", loadHandler)
 
 	fmt.Println("API up and running on http://localhost" + port + "/api/")
 	log.Fatal(http.ListenAndServe(port, apiServer))
@@ -56,4 +59,21 @@ func moveSequenceHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	MoveRover(movementAction, request, responseHandler)
+}
+
+func loadHandler(w http.ResponseWriter, r *http.Request) {
+	responseHandler := NewResponseHandler(w)
+	if r.Method != "POST" {
+		responseHandler.SendBadRequest("Invalid method")
+		return
+	}
+
+	var request LoadRequest
+	err := json.NewDecoder(r.Body).Decode(&request)
+	if err != nil {
+		responseHandler.SendBadRequest("Invalid request body")
+		return
+	}
+
+	LoadGame(loadAction, request, responseHandler)
 }
